138c0b6d9d04a3dc33ce9a1e6df7b99af2559a27
Dilraj <wolfstrix360@gmail.com>
2025-07-23 22:31:17 +0530

    commit

--- client/src/api.js
+++ client/src/api.js
@@ -8,7 +8,6 @@ const API = axios.create({
 export const register = (data) => API.post('/auth/register', data);
 export const login = (data) => API.post('/auth/login', data);
 
-// Task API functions
 export const getTasks = () => API.get('/tasks');
 
 
